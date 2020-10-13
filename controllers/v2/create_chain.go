828733e044c4b4416b64fe57832160359f76bc4c
chunqizhi <1558763837@qq.com>
2020-10-13 12:49:55 +0000

    Modify create_chain.go

--- controllers/v2/create_chain.go
+++ controllers/v2/create_chain.go
@@ -4,13 +4,10 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/jason-cn-dev/xuper-sdk-go/account"
-	"github.com/jason-cn-dev/xuper-sdk-go/pb"
 	"github.com/jason-cn-dev/xuper-sdk-go/transfer"
-	"github.com/jason-cn-dev/xuper-sdk-go/txhash"
 	"github.com/jason-cn-dev/xupercc/conf"
 	"github.com/jason-cn-dev/xupercc/controllers"
 	log "github.com/jason-cn-dev/xupercc/utils"
-	"math/big"
 	"net/http"
 	"strconv"
 	"strings"
@@ -26,19 +23,6 @@ type Args struct {
 	Name string `json:"name"`
 	Data string `json:"data"`
 }
-
-// TODO
-type ParamJSON struct {
-	Version   string `json:"version"`
-	Consensus struct {
-		Miner string `json:"miner"`
-	} `json:"consensus"`
-	Predistribution []struct {
-		Address string `json:"address"`
-		Quota   string `json:"quota"`
-	} `json:"predistribution"`
-}
-// TODO
 
 func CreateChain(c *gin.Context) {
 
@@ -77,31 +61,6 @@ func CreateChain(c *gin.Context) {
 		Args:   args,
 	}
 
-	// TODO
-	paramJson := []byte(args.Data)
-	jsObj := &ParamJSON{}
-	jsErr := json.Unmarshal(paramJson, jsObj)
-	if jsErr != nil {
-		return
-	}
-	utxoTx := &pb.Transaction{Version: 0}
-	// 暂时只支持配置给一个初始化账户打钱
-	for _, pd := range jsObj.Predistribution {
-		amount := big.NewInt(0)
-		amount.SetString(pd.Quota, 10) // 10进制转换大整数
-		if amount.Cmp(big.NewInt(0)) < 0 {
-			return
-		}
-		txOutput := &pb.TxOutput{}
-		txOutput.ToAddr = []byte(pd.Address)
-		txOutput.Amount = amount.Bytes()
-		utxoTx.TxOutputs = append(utxoTx.TxOutputs, txOutput)
-
-		// 暂时只支持配置给一个初始化账户打钱
-		break
-	}
-	// TODO
-
 	bytes, err := json.Marshal(&desc)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -111,22 +70,6 @@ func CreateChain(c *gin.Context) {
 		log.Printf("json Marshal fail, err: %s", err.Error())
 		return
 	}
-
-	// TODO
-	utxoTx.Desc = bytes
-	utxoTx.Coinbase = true
-	utxoTx.Txid, _ = txhash.MakeTransactionID(utxoTx)
-	txid1, err := trans.PostTx(utxoTx)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"code":  400,
-			"msg":   "创建平行链",
-			"error": err.Error(),
-		})
-		log.Printf("transfer fail, err: %s", err.Error())
-		return
-	}
-	log.Printf("create block chain successfully,txid: %s ",txid1)
 
 	to := args.Name
 	amount := trans.Cfg.MinNewChainAmount
